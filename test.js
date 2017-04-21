5e96b964cce04b22d542f3a900ae6060d956fa83
shenqiangu <shenqiangu@qq.com>
2017-04-21 16:12:30 +0800

    more kexue

--- test.js
+++ test.js
@@ -2,3 +2,14 @@ var jisuanqi = require('./index')
 
 console.log(jisuanqi.add(1, 2))
 console.log(jisuanqi.kexue('2+2*2'))
+
+require('mathjs').eval = undefined
+
+try {
+    console.log(jisuanqi.kexue('3+3'));
+} catch (err) {
+    console.error(err)
+}
+
+var kexue = require('./dist/jisuanqi.js')
+console.log(kexue.kexue('1+1*6'))
